702e079e8f0090c54c2b8c810f46291a1efc88ea
Abdulla Haseeb <hsb@Abdullas-MacBook-Air.local>
2024-05-01 23:33:57 +0400

    home screen componentes updated

--- src/view/screens/HomeScreen.jsx
+++ src/view/screens/HomeScreen.jsx
@@ -1,10 +1,61 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
+import {Images} from '../theme/image';
+import {useNavigation} from '@react-navigation/native';
+
+const data = [
+  {title: 'Agenda', imageUrl: Images.Agenda},
+  {title: 'Speakers', imageUrl: Images.Speakers},
+  {title: 'Badge', imageUrl: Images.Badge},
+  {title: 'Venue', imageUrl: Images.Venue},
+  {title: 'Brand Innovation', imageUrl: Images.BrandInnovations},
+  {title: 'Brand Videos', imageUrl: Images.Videos},
+  {title: 'Ask Questions', imageUrl: Images.Questions},
+  {title: 'Voting', imageUrl: Images.Voting},
+  {title: 'Social Media', imageUrl: Images.Social},
+  {title: 'Survey', imageUrl: Images.Survey},
+  {title: 'CME', imageUrl: Images.CME},
+  {title: 'More', imageUrl: Images.More},
+];
+
+const itemColors = ['#0ea5e9', '#94a3b8', '#075985'];
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const renderItem = ({item, index}) => {
+    const colorIndex = index % 3;
+    const bgColor = itemColors[colorIndex];
+    return (
+      <View className="w-[50%] px-2">
+        <View
+          style={{aspectRatio: 1, backgroundColor: bgColor}}
+          className="  rounded-[20px] ">
+          <TouchableOpacity
+            onPress={() => navigation.navigate('AgendaScreen')}
+            className="w-full h-full items-center justify-center">
+            <Image source={item.imageUrl} className="w-full h-full" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>hello</Text>
+    <View className="flex-1">
+      <FlatList
+        data={data}
+        keyExtractor={(_, index) => index.toString()}
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+        renderItem={renderItem}
+        contentContainerStyle={{
+          paddingVertical: 8,
+          flexGrow: 1,
+          paddingBottom: 40,
+        }}
+        columnWrapperStyle={{padding: 8}}
+      />
     </View>
   );
 };
---
+++ src/view/theme/image.js
@@ -0,0 +1,37 @@
+import Agenda from '../assets/images/agenda.png';
+import Badge from '../assets/images/badge.png';
+import CME from '../assets/images/cme.png';
+import Home from '../assets/images/home.png';
+import Lldd from '../assets/images/lldd.png';
+import More from '../assets/images/more.png';
+import Questions from '../assets/images/questions.png';
+import SnapChat from '../assets/images/snapchat.png';
+import Speakers from '../assets/images/speakers.png';
+import Survey from '../assets/images/survey.png';
+import Venue from '../assets/images/venue.png';
+import Voting from '../assets/images/voting.png';
+import Videos from '../assets/images/videos.png';
+import BrandInnovations from '../assets/images/images.png';
+import Social from '../assets/images/social.png';
+
+const TitleImage = {
+  Agenda,
+  Badge,
+  CME,
+  Home,
+  Lldd,
+  More,
+  Questions,
+  SnapChat,
+  Speakers,
+  Survey,
+  Venue,
+  Voting,
+  Videos,
+  BrandInnovations,
+  Social,
+};
+
+export const Images = {
+  ...TitleImage,
+};
